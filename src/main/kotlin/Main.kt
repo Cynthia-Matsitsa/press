b869096173bb9bfeea2e8672338b5ba2cd06bef0
Cynthia-Matsitsa <cynthiamatsitsa@gmail.com>
2024-03-25 08:15:55 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+     println(sentence("Barnie","bakes","brown","bagels","buns"))
+    isPandrome()
+
+}
+fun sentence(b1:String,b2:String,b3:String,b4:String,b5:String){
+    var words = "Barnie bakes brown bagels and buns"
+
+}
+fun isPalindrome(word:String):Boolean{
+    var words = "madam wow kayak"
+    if(words == "gender" ){
+        println("madam wow kayak")
+
+
+
+
+    }
+
+
+
+}
